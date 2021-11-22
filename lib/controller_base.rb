a6f232298f947f59e8afc79fd81b8a3dbeffb688
sam <s.gelernter@gmail.com>
2021-11-22 16:54:38 -0500

    Begin phase IV

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -6,6 +6,7 @@ require_relative './session'
 require 'byebug'
 
 class ControllerBase
+  
   attr_reader :req, :res, :params
   attr_writer :already_built_response
 
@@ -26,7 +27,7 @@ class ControllerBase
     raise 'double render error' if already_built_response?
     res.status = 302
     res.location = url
-    already_built_response = true
+    self.already_built_response = true
   end
 
   # Populate the response with content.
@@ -36,17 +37,17 @@ class ControllerBase
     raise 'double render error' if already_built_response?
     res.write(content) 
     res.content_type = content_type 
-    already_built_response = true 
+    self.already_built_response = true 
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
     raise 'double render error' if already_built_response?
-    debugger
     controller_name = self.class.to_s
     controller_path_name = controller_name.underscore
-    full_path = "views/#{controller_path_name}/#{template_name}.html.erb"
+    top_path = File.dirname(__FILE__)
+    full_path = "#{top_path}/../views/#{controller_path_name}/#{template_name}.html.erb"
     content = File.read(full_path)
     parsed_content = ERB.new(content).result(binding)
     render_content(parsed_content, 'text/html')
--- lib/session.rb
+++ lib/session.rb
@@ -3,7 +3,11 @@ require 'json'
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
+  attr_reader :rails_cookie 
+
   def initialize(req)
+    @rails_cookie = req.cookies["_rails_lite_app"]
+
   end
 
   def [](key)
@@ -15,5 +19,6 @@ class Session
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    Rack::Response.set_cookie(rails_cookie)
   end
 end
