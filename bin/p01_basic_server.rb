d0e7bb72771f5e3c18416d04e48740abb897759a
sam <s.gelernter@gmail.com>
2021-11-22 14:47:28 -0500

    Complete path return server

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,16 @@
 require 'rack'
+require 'byebug'
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    path_string = req.path
+    res = Rack::Response.new 
+    res['Content-Type'] = 'text/html'
+    res.write(path_string)
+    res.finish 
+end
+
+Rack::Server.start(
+    app: app,
+    Port: 3000
+)
